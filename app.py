2f992a1d52c93a0ab7b93bc4118bc959ad459009
Sanskar Sisodia <500091107@stu.upes.ac.in>
2024-03-31 10:42:10 +0530

    NLP PROJECT ADDED

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
+import os
 import pickle
 from flask import Flask, render_template, request
 import re
 import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
 
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
 app = Flask(__name__)
-
-# Load pickled files & data
-with open('count_vectorizer.pkl', 'rb') as f:
-    cv = pickle.load(f)
-
-with open('tfidf_transformer.pkl', 'rb') as f:
-    tfidf_transformer = pickle.load(f)
 
 with open('feature_names.pkl', 'rb') as f:
     feature_names = pickle.load(f)
 
 # Cleaning data:
 stop_words = set(stopwords.words('english'))
-new_stop_words = ["fig","figure","image","sample","using",
-             "show", "result", "large",
-             "also", "one", "two", "three",
-             "four", "five", "seven","eight","nine"]
+new_stop_words = ["fig", "figure", "image", "sample", "using",
+                  "show", "result", "large",
+                  "also", "one", "two", "three",
+                  "four", "five", "seven", "eight", "nine"]
 stop_words = list(stop_words.union(new_stop_words))
 
 def preprocess_text(txt):
@@ -45,25 +44,6 @@ def preprocess_text(txt):
 
     return " ".join(txt)
 
-def sort_coo(coo_matrix):
-    tuples = zip(coo_matrix.col, coo_matrix.data)
-    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
-
-def extract_topn_from_vector(feature_names, sorted_items, topn=10):
-    sorted_items = sorted_items[:topn]
-
-    score_vals = []
-    feature_vals = []
-    for idx, score in sorted_items:
-        fname = feature_names[idx]
-        score_vals.append(round(score, 3))
-        feature_vals.append(feature_names[idx])
-
-    results = {}
-    for idx in range(len(feature_vals)):
-        results[feature_vals[idx]] = score_vals[idx]
-    return results
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -76,11 +56,25 @@ def extract_keywords():
 
     if document:
         text = document.read().decode('utf-8', errors='ignore')
-        preprocessed_text = preprocess_text(text)
-        tf_idf_vector = tfidf_transformer.transform(cv.transform([preprocessed_text]))
-        sorted_items = sort_coo(tf_idf_vector.tocoo())
-        keywords = extract_topn_from_vector(feature_names, sorted_items, 20)
+        cleaned_text = preprocess_text(text)
+
+        # Initialize TfidfVectorizer
+        tfidf_vectorizer = TfidfVectorizer()
+
+        # Fit and transform the preprocessed text
+        tfidf_matrix = tfidf_vectorizer.fit_transform([cleaned_text])
+
+        # Get feature names
+        feature_names = tfidf_vectorizer.get_feature_names_out()
+
+        # Get TF-IDF vector for the cleaned text
+        tfidf_vector = tfidf_matrix.toarray()[0]
+
+        # Combine feature names with their corresponding TF-IDF values
+        keywords = dict(zip(feature_names, tfidf_vector))
+
         return render_template('keywords.html', keywords=keywords)
+
     return render_template('index.html')
 
 @app.route('/search_keywords', methods=['POST'])
